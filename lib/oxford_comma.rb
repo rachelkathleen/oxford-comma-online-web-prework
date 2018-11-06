b60f086d40b41b7533162d376ae012febbbec6e2
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 22:33:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+  def oxford_comma(array)
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array.join(" and ")
+    else
+      array.last = last_item
+      
+    end
+  end
