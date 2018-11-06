5017b18dbdd04729cead0f7f34daa860711056b1
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 22:39:19 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+      array.join
+    elsif array.length == 2
+      array.join(" and ")
+    else
+      last_item = array.last
+      and_last_item = "and " + last_item
+      array.pop
+      array << and_last_item
+      array.join(", ")
+    end
+  end
